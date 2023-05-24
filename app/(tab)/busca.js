bd1729c52b118657a418cbe3b342e7a710ad6508
pedroHteles <pedroteles849@gmail.com>
2023-05-24 00:56:52 -0300

    v1

--- app/(tab)/busca.js
+++ app/(tab)/busca.js
@@ -1,21 +1,7 @@
 import React from 'react';
-import { View, TextInput, Button, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import {  Text } from 'react-native';
 
-import
-  {
-    Container,
-    OptionsList,
-    Option,
-    Info,
-    Title,
-    Description,
-    Wrapper,
-    AdditionalMenu,
-    AdditionalOption,
-    OptionName
-  } from './styles';
 
-// navigation prop
 export default function Home({ navigation }) {
   return (
    <>
