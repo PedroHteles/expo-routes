fa9150172d7cfaafd9cf017471ed42f31780c05a
pedroHteles <pedroteles849@gmail.com>
2023-05-24 00:43:52 -0300

    first commit

---
+++ app/(tab)/pedidos.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, TextInput, Button, TouchableOpacity, Text, StyleSheet } from 'react-native';
+
+import
+  {
+    Container,
+    OptionsList,
+    Option,
+    Info,
+    Title,
+    Description,
+    Wrapper,
+    AdditionalMenu,
+    AdditionalOption,
+    OptionName
+  } from './styles';
+
+// navigation prop
+export default function Home({ navigation }) {
+  return (
+   <>
+   <Text>Pedidos</Text>
+   </>
+  );
+}
